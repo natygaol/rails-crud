fcee91b9e72a5470637f3a6fbd81473de24d938d
nataly garcia <naty_garcioli@hotmail.com>
2025-09-03 22:07:25 -0500

    add address to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_04_030526) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_04_030707) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
